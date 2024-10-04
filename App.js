74124e9f13a8077aaf8e8e3d26c3b2ca5e4dc3da
trilvid <trillztech34@gmail.com>
2024-10-05 04:49:12 +1200

    updated data now bruhx

--- App.js
+++ App.js
@@ -20,6 +20,11 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use(cors())
+app.use(cors({
+  origin: '*',  // Allow all origins
+  methods: 'GET,POST,PUT,DELETE,PATCH',
+  allowedHeaders: 'Content-Type, Authorization'
+}));
 app.use(express.json({ limit: '10mb' }));
 app.use(fileUpload({
     limits: { fileSize: 50 * 1024 * 1024 }
