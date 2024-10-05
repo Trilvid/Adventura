fc9dfe61adc6c38e7affbecf9abce005ab9f166c
trilvid <trillztech34@gmail.com>
2024-10-06 03:04:04 +1200

    fuck it

--- App.js
+++ App.js
@@ -15,9 +15,9 @@ const path = require('path');
 const app = express();
 
 // 1) MIDDLEWARES
-// if (process.env.NODE_ENV === 'development') {
-//   app.use(morgan('dev'));
-// }
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(cors())
 app.use(express.json({ limit: '10mb' }));
